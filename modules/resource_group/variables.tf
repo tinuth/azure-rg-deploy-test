fcfb4e075dd1d3fcdcdeb41cc1e7f32dd011944c
Tinu <tinuthomas695@gmail.com>
2025-07-03 13:27:14 +0000

    This is my RG commit

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,6 +1,6 @@
 
 variable "name" {
-  description  =  "This is variable name defination for module resourc3_group"
+  description  =  "This is variable name defination for module resource_group"
   type         =  string
 }
 variable "location"{
